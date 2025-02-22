81deeb8287a5df92617703e747f10706e6cf6b18
Alberto Aurensanz <aaurensanz@hiberus.com>
2025-02-22 13:18:39 +0100

    2-oscilador tabla

--- 2-oscilador/analisis.m
+++ 2-oscilador/analisis.m
@@ -3,7 +3,7 @@ t = readtable('files/data/1-y.csv');
 x = t.m;
 y = t.y_y0;
 
-scatter(x, y, 40, "filled", "black")
+scatter(x, y, 50, "filled", "black")
 set(gca,'fontsize', 14);
 lab1 = ylabel('$y-y_0$ (mm)'); 
 lab2 = xlabel('$m$ (g)');
@@ -13,7 +13,11 @@ set(lab2,'Interpreter','latex', 'FontSize', 20);
 P = polyfit(x,y,1);
 yfit = P(1)*x+P(2);
 hold on;
-plot(x,yfit,'r--.', 'Color', 'Black');
+h = plot(x,yfit,'r--.', 'Color', 'Black');
+
+gravstr = sprintf('$y = %.3f x %.3f$ ',P(1), P(2));
+l = legend(gravstr)
+set(l,'Interpreter','latex', 'FontSize', 20);
 
 fig1 = gca;
 exportgraphics(fig1,'files/images/fig1.eps','Resolution',300);
