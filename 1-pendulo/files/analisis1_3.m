6a623d62a725b446d273f9da1a71b5cd2b92b001
Alberto Aurensanz <aaurensanz@hiberus.com>
2025-03-08 19:59:02 +0100

    P1 - Add pendulum diagram

--- 1-pendulo/files/analisis1_3.m
+++ 1-pendulo/files/analisis1_3.m
@@ -3,26 +3,29 @@ clc;
 close all;
 
 t = readtable('data/1-t2-20.csv');
-x = sqrt(t.l / 1000);
-y = t.T ;
+y = t.l / 1000;
+x = t.T ;
 
 scatter(x, y, 50, "filled", "black");
+set(gca,'xscale','log', 'YScale', 'log');
+grid on;
 set(gca,'fontsize', 14);
-lab1 = ylabel('$T$\,(s)'); 
-lab2 = xlabel('$\sqrt{l}$\,(m$^{1/2}$)');
+lab1 = xlabel('$T$\,(s)'); 
+lab2 = ylabel('$l$\,(m)');
 set(lab1,'Interpreter','latex', 'FontSize', 20);
 set(lab2,'Interpreter','latex', 'FontSize', 20);
 
-P = polyfit(x,y,1);
-yfit = P(1)*x+P(2);
+P = polyfit(log10(x),log10(y),1);
+yfit = P(1)*log10(x)+P(2);
 hold on;
-h = plot(x,yfit,'r--', 'Color', 'Black');
+h = loglog(x,10.^(yfit),'r--', 'Color', 'Black');
 
-gravstr = sprintf('$y = %.3f x %.3f$ ',P(1), P(2));
+gravstr = sprintf('$y = %.4f x %.4f$ ',P(1), P(2));
 l = legend(gravstr, 'Location','north');
 set(l,'Interpreter','latex', 'FontSize', 20);
 
 % obtener medida de la bondad del ajuste
-mdl = fitlm(x,y)
+mdl = fitlm(log10(x),log10(y))
+
 
 
