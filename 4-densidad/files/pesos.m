079f6c95f9d929dec29478e0010ff5840b796368
Alberto Aurensanz <aaurensanz@hiberus.com>
2025-03-16 13:48:49 +0100

    P4 - Análisis

--- 4-densidad/files/pesos.m
+++ 4-densidad/files/pesos.m
@@ -4,27 +4,27 @@ close all;
 
 t = readtable('data/2.csv')
 x = t.n;
-y = t.P_v;
+%y = t.P_v;
 %y = t.P_a;
-%y = t.P_v ./ (t.P_v - t.P_a);
+y = t.P_v ./ (t.P_v - t.P_a);
 
 scatter(x, y, 50, "filled", "black");
 set(gca,'fontsize', 14);
-lab1 = ylabel('$P_{vac}$\,(N)'); 
+%lab1 = ylabel('$P_{vac}$\,(N)'); 
 %lab1 = ylabel('$P_{agua}$\,(N)'); 
-%lab1 = ylabel('$P_{vac}/(P_{vac} - P_{agua})$\,(N)'); 
+lab1 = ylabel('$P_{vac}/(P_{vac} - P_{agua})$\,(N)'); 
 lab2 = xlabel('$n$');
 set(lab1,'Interpreter','latex', 'FontSize', 20);
 set(lab2,'Interpreter','latex', 'FontSize', 20);
 
-P = polyfit(x,y,1);
-yfit = P(1)*x+P(2);
-hold on;
-h = plot(x,yfit,'r--', 'Color', 'Black');
+%P = polyfit(x,y,1);
+%yfit = P(1)*x+P(2);
+%hold on;
+%h = plot(x,yfit,'r--', 'Color', 'Black');
 
-gravstr = sprintf('$y = %.3f x + %.3f$ ',P(1), P(2));
-l = legend(gravstr, 'Location','north');
-set(l,'Interpreter','latex', 'FontSize', 20);
+%gravstr = sprintf('$y = %.3f x + %.3f$ ',P(1), P(2));
+%l = legend(gravstr, 'Location','north');
+%set(l,'Interpreter','latex', 'FontSize', 20);
 
 % obtener medida de la bondad del ajuste
 mdl = fitlm(x,y)
