ee741b094019aff75ae3ee51e09608b9efd82289
Alberto Aurensanz <aaurensanz@hiberus.com>
2025-03-25 22:38:14 +0100

    P3 - Repaso

--- 3-viscosidad/files/cuestion2.m
+++ 3-viscosidad/files/cuestion2.m
@@ -1,8 +1,8 @@
 syms t rho;
 
-eta = 0.0840;
+eta = 0.084;
 g = 9.81;
-r = 0.00242;
+r = 0.0022;
 %rho_b = 1000;
 rho_b = 7850;
 alpha = 9*eta / (2 * r^2 * rho_b);
@@ -10,6 +10,6 @@ beta = (1-rho/rho_b)* g / alpha^2;
 z_0 = 0.2;
 syms z(t,rho);
 z = z_0 + beta* (1 - exp(-alpha*t)-alpha*t);
-eval(subs(z, [t,rho], [0.3012,920]))
+eval(subs(z, [t,rho], [0.326,920]))
 
 
