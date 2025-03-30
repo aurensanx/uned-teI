6581eafcfb8823b43cb8342a31eb4c44d3138986
Alberto Aurensanz <aaurensanz@hiberus.com>
2025-03-30 16:45:28 +0200

    P6 - Tablas y gráficas iniciales

---
+++ 5-resistencia/files/resistencia.m
@@ -0,0 +1,28 @@
+clear;
+clc;
+close all;
+
+t = readtable('data/0.32 mm^2 - 2m.csv')
+x = t.Var1;
+y = t.Var2;
+
+scatter(x, y, 50, "filled", "black");
+set(gca,'fontsize', 14);
+lab1 = ylabel('$\Delta V$\,(mV)'); 
+lab2 = xlabel('$I\,$(mA)');
+set(lab1,'Interpreter','latex', 'FontSize', 20);
+set(lab2,'Interpreter','latex', 'FontSize', 20);
+
+P = polyfit(x,y,1);
+yfit = P(1)*x+P(2);
+hold on;
+h = plot(x,yfit,'r--', 'Color', 'Black');
+
+gravstr = sprintf('$y = %.4f x  %.4f$ ',P(1), P(2));
+l = legend(gravstr, 'Location','north');
+set(l,'Interpreter','latex', 'FontSize', 20);
+
+% obtener medida de la bondad del ajuste
+mdl = fitlm(x,y)
+
+
