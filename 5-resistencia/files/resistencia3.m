dae5d94699d5d5508152e8421c8b909b761f0e8e
Alberto Aurensanz <aaurensanz@hiberus.com>
2025-03-31 21:10:57 +0200

    5 - Análisis 4

---
+++ 5-resistencia/files/resistencia3.m
@@ -0,0 +1,27 @@
+clear;
+clc;
+close all;
+
+t = readtable('data/RS.csv')
+x = 1./t.Var1;
+y = t.Var4;
+scatter(x, y, 50, "filled", "black");
+
+set(gca,'fontsize', 14);
+
+lab1 = ylabel('$R$\,(m$\Omega$)'); 
+lab2 = xlabel('$1/S\,$(mm$^{-2}$)');
+set(lab1,'Interpreter','latex', 'FontSize', 20);
+set(lab2,'Interpreter','latex', 'FontSize', 20);
+
+P = polyfit(x,y,1);
+yfit = P(1)*x+P(2);
+hold on;
+h = plot(x,yfit,'r--', 'Color', 'Black');
+
+gravstr = sprintf('$y = %.0f x + %.0f$ ',P(1), P(2));
+l = legend(gravstr, 'Location','north');
+set(l,'Interpreter','latex', 'FontSize', 20);
+
+% obtener medida de la bondad del ajuste
+mdl = fitlm(x,y)
