f7c1adf098ad952717c4631ae60e8d5bef5ddc0b
Alberto Aurensanz <aaurensanz@hiberus.com>
2025-04-04 18:14:08 +0200

    6 - 2º experimento

--- 6-reflexión-y-refracción/files/segundo.m
+++ 6-reflexión-y-refracción/files/segundo.m
@@ -28,25 +28,16 @@ lab2 = xlabel('$\theta_i\,(^{\circ})$');
 set(lab1,'Interpreter','latex', 'FontSize', 20);
 set(lab2,'Interpreter','latex', 'FontSize', 20);
 
-P = polyfit(x,y,1);
-yfit = P(1)*x+P(2);
+%P = polyfit(x,y,1);
+%yfit = P(1)*x+P(2);
 hold on;
-h = plot(x,yfit,'r--', 'Color', 'Black');
+%h = plot(x,yfit,'r--', 'Color', 'Black');
+plot(x, 0.676*x-0.001,'r--', 'Color', 'Black')
 
-gravstr = sprintf('$y = %.2f x  %.2f$ ',P(1), P(2));
-l = legend(gravstr, 'Location','north');
-set(l,'Interpreter','latex', 'FontSize', 20);
+%gravstr = sprintf('$y = %.3f x  %.3f$ ',P(1), P(2));
+%l = legend(gravstr, 'Location','north');
+%set(l,'Interpreter','latex', 'FontSize', 20);
 
 % obtener medida de la bondad del ajuste
 mdl = fitlm(x,y)
 
-
-
-
-% 1 reflexión
-%lab1 = ylabel('$\theta_r\,(^{\circ})$'); 
-%lab2 = xlabel('$\theta_i\,(^{\circ})$');
-
-% 2 refracción
-%lab1 = ylabel('sen$$\,\theta_t$');
-%lab2 = xlabel('sen$\,\theta_i$');
